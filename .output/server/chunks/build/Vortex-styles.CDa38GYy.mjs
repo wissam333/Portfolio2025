4e575a029aee1b1d67b8b2952983aa65567d1885
Wissam_Najjom <wissam.n.najjom@gmail.com>
2025-10-20 12:57:56 +0300

    2025/10/20 ر2

---
+++ .output/server/chunks/build/Vortex-styles.CDa38GYy.mjs
@@ -0,0 +1,6 @@
+const Vortex_vue_vue_type_style_index_0_scoped_2eabbed8_lang = ".vortex-container[data-v-2eabbed8]{height:100%;position:relative;width:100%}.canvas[data-v-2eabbed8]{height:100%;top:0;right:0;bottom:0;left:0;position:absolute;width:100%;z-index:1!important}.canvas canvas[data-v-2eabbed8]{display:block;height:100%!important;width:100%!important}";
+
+const VortexStyles_CDa38GYy = [Vortex_vue_vue_type_style_index_0_scoped_2eabbed8_lang];
+
+export { VortexStyles_CDa38GYy as default };
+//# sourceMappingURL=Vortex-styles.CDa38GYy.mjs.map
